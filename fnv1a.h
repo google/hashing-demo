6f6428133a7ee01cc819a53adb28ffdf1b5b57c7
Geoff Romer <gromer@google.com>
2015-03-18 08:54:53 -0700

    Initial checkin- code compiles, but isn't instantiated or tested.

---
+++ fnv1a.h
@@ -0,0 +1,139 @@
+#ifndef EXPERIMENTAL_GROMER_FNV1A_H
+#define EXPERIMENTAL_GROMER_FNV1A_H
+
+#include "std.h"
+
+namespace hashing {
+
+// Implementation of FNV-1a hash algorithm, based on the implementation
+// in N3980.
+class fnv1a {
+  std::size_t state_ = 14695981039346656037u;
+
+ public:
+  using result_type = size_t;
+
+  // Generic recursive case of hash_combine.
+  template <typename T, typename... Ts>
+  friend std::enable_if_t<!std::is_uniquely_represented_v<T>,
+                          fnv1a>
+  hash_combine(fnv1a hash_code, const T& value, const Ts&... values) {
+    return hash_combine(hash_value(hash_code, value), values...);
+  }
+
+  // Base case of hash_combine: hash the bytes directly once we reach a
+  // uniquely-represented type.
+  template <typename T, typename... Ts>
+  friend std::enable_if_t<std::is_uniquely_represented_v<T>,
+                          fnv1a>
+  hash_combine(fnv1a hash_code, const T& value, const Ts&... values) {
+    unsigned char const* bytes = static_cast<unsigned char const*>(&value);
+    return hash_combine(
+        hash_combine_range(hash_code, bytes, bytes + sizeof(value)), values...);
+  }
+
+  // Base case of variadic template recursion.
+  friend fnv1a hash_combine(fnv1a hash_code) { return hash_code; }
+
+  // Generic iterative implementation of hash_combine_range.
+  template <typename InputIterator>
+  // Avoid ambiguity with the following overload
+  friend std::enable_if_t<
+      !(std::is_contiguous_iterator<InputIterator>::value &&
+        std::is_uniquely_represented<
+            typename std::iterator_traits<InputIterator>::value_type>::value),
+      fnv1a>
+  hash_combine_range(fnv1a hash_code, InputIterator begin, InputIterator end) {
+    while (begin != end) {
+      hash_code = hash_value(hash_code, *begin);
+      ++begin;
+    }
+    return hash_code;
+  }
+
+  // Overload for a contiguous sequence of a uniquely-represented type: hash
+  // the bytes directly. This is an optimization; the overload above would
+  // work in these cases as well, but will probably be much less efficient.
+  template <typename InputIterator>
+  friend std::enable_if_t<
+      std::is_contiguous_iterator<InputIterator>::value &&
+          std::is_uniquely_represented<
+              typename std::iterator_traits<InputIterator>::value_type>::value,
+      fnv1a>
+  hash_combine_range(fnv1a hash_code, InputIterator begin, InputIterator end) {
+    const unsigned char* begin_ptr =
+        static_cast<const unsigned char*>(adl_pointer_from(begin));
+    const unsigned char* end_ptr =
+        static_cast<const unsigned char*>(adl_pointer_from(end));
+    while (begin < end) {
+      hash_code.state_ = mix(hash_code, *begin);
+      ++begin;
+    }
+    return hash_code;
+  }
+
+  explicit operator result_type() noexcept { return state_; }
+
+ private:
+  static size_t mix(fnv1a hash_code, unsigned char c) {
+    return (hash_code.state_ ^ c) * 1099511628211u;
+  }
+};
+
+// Type-invariant implementation of FNV-1a hash algorithm. In order to
+// provide the type-invariance property, we have to forego the
+// optimization for uniquely-represented types, because different
+// types may use different internal representations of the same value.
+class type_invariant_fnv1a {
+  std::size_t state_ = 14695981039346656037u;
+
+ public:
+  using result_type = size_t;
+
+  template <typename T, typename... Ts>
+  friend type_invariant_fnv1a hash_combine(
+      type_invariant_fnv1a hash_code, const T& value, const Ts&... values) {
+    return hash_combine(hash_value(hash_code, value), values...);
+  }
+
+  template <typename... Ts>
+  friend type_invariant_fnv1a hash_combine(
+      type_invariant_fnv1a hash_code, unsigned char c, const Ts&... values) {
+    return hash_combine(mix(hash_code, c), values...);
+  }
+
+  friend type_invariant_fnv1a hash_combine(type_invariant_fnv1a hash_code) {
+    return hash_code;
+  }
+
+  template <typename InputIterator>
+  friend type_invariant_fnv1a hash_combine_range(
+      type_invariant_fnv1a hash_code, InputIterator begin, InputIterator end) {
+    while (begin != end) {
+      hash_code = hash_value(hash_code, *begin);
+      ++begin;
+    }
+    return hash_code;
+  }
+
+  friend type_invariant_fnv1a hash_combine_range(
+      type_invariant_fnv1a hash_code, unsigned char const* begin,
+      unsigned char const* end) {
+    while (begin < end) {
+      hash_code.state_ = mix(hash_code, *begin);
+      ++begin;
+    }
+    return hash_code;
+  }
+
+  explicit operator result_type() noexcept { return state_; }
+
+ private:
+  static size_t mix(type_invariant_fnv1a hash_code, unsigned char c) {
+    return (hash_code.state_ ^ c) * 1099511628211u;
+  }
+};
+
+}  // namespace hashing
+
+#endif  // EXPERIMENTAL_GROMER_FNV1A_H
---
+++ fnv1a_test.cc
@@ -0,0 +1,6 @@
+#include "std.h"
+#include "fnv1a.h"
+
+int main(int argc, const char* argv[]) {
+  return 0;
+}
