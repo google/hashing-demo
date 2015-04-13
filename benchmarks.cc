a983851cf2d74ed9c1b2ec50fe41f011070de254
Geoff Romer <gromer@google.com>
2015-04-13 13:19:41 -0700

    Add benchmark for hashing non-contiguous data.

--- benchmarks.cc
+++ benchmarks.cc
@@ -94,4 +94,65 @@ BENCHMARK_TEMPLATE(BM_HashStrings, std::hasher<string_piece, hashing::farmhash>)
 BENCHMARK_TEMPLATE(BM_HashStrings, std::uhash<hashing::n3980::farmhash>)
     ->Range(1, 1000 * 1000);
 
+// Based on N3980's "X", but data_ is non-contiguous, in order to exercise
+// a different part of the performance space.
+struct X {
+  std::tuple<short, unsigned char, unsigned char> date_;
+  std::vector<std::pair<char, int>>                data_;
+};
+
+template <typename HashCode>
+HashCode hash_decompose(HashCode code, const X& x) {
+  return hash_combine(std::move(code), x.date_, x.data_);
+}
+
+template <typename HashAlgorithm>
+void hash_append(HashAlgorithm& h, const X& x) {
+  using std::hash_append;
+  hash_append(h, x.date_, x.data_);
+}
+
+template <class H>
+static void BM_HashX(benchmark::State& state) {
+  static std::uniform_int_distribution<short> years(1915, 2015);
+  static std::uniform_int_distribution<unsigned char> months(1, 12);
+  static std::uniform_int_distribution<unsigned char> days(1, 28);
+  static std::uniform_int_distribution<char> cvalue(1, 10);
+  static std::uniform_int_distribution<int> ivalue(-3, 3);
+
+  static std::default_random_engine engine;
+
+  const int max_data_size = state.range_x();
+  std::uniform_int_distribution<std::size_t> data_size(0, max_data_size);
+
+  const int num_xs = kNumBytes/max_data_size;
+  std::vector<X> xs(num_xs);
+
+  for (X& x : xs) {
+    x.date_ = {years(engine), months(engine), days(engine)};
+    x.data_.resize(data_size(engine));
+    for (auto& p : x.data_) {
+      p = {cvalue(engine), ivalue(engine)};
+    }
+  }
+
+  int i = 0;
+  int64_t cumulative_vector_size = 0;
+  H h;
+  while (state.KeepRunning()) {
+    benchmark::DoNotOptimize(h(xs[i]));
+    i = (i + 1) % xs.size();
+    cumulative_vector_size += xs[i].data_.size();
+  }
+  state.SetBytesProcessed(
+      static_cast<int64_t>(state.iterations()) * sizeof(X)
+      + cumulative_vector_size * sizeof(std::pair<int, int>));
+}
+
+BENCHMARK_TEMPLATE(BM_HashX, std::hasher<X, hashing::farmhash>)
+    ->Range(1, 1000 * 1000);
+
+BENCHMARK_TEMPLATE(BM_HashX, std::uhash<hashing::n3980::farmhash>)
+    ->Range(1, 1000 * 1000);
+
 BENCHMARK_MAIN();
--- n3980.h
+++ n3980.h
@@ -22,6 +22,8 @@
 #include <string>
 #include <type_traits>
 
+#include "std.h"
+
 namespace std {
 
 template <typename HashAlgorithm, typename Integral>
@@ -34,6 +36,70 @@ template <typename HashAlgorithm>
 void hash_append(HashAlgorithm& h, const string& str) {
   h(str.data(), str.size());
   hash_append(h, str.size());
+}
+
+template <typename HashAlgorithm, typename T1, typename T2>
+void hash_append(HashAlgorithm& h, const pair<T1, T2>& p) {
+  hash_append(h, p.first);
+  hash_append(h, p.second);
+}
+
+template <typename HashAlgorithm, typename T, typename... Ts>
+void hash_append(HashAlgorithm& h, const T& value, const Ts&... values) {
+  hash_append(h, value);
+  hash_append(h, values...);
+}
+
+namespace detail {
+template <typename HashAlgorithm, typename Tuple, size_t... Is>
+void hash_append_tuple(
+    HashAlgorithm& h, const Tuple& t, index_sequence<Is...>) {
+  hash_append(h, get<Is>(t)...);
+}
+}  // namespace detail
+
+template <typename HashAlgorithm, typename... Ts>
+void hash_append(HashAlgorithm& h, const tuple<Ts...>& t) {
+  detail::hash_append_tuple(h, t, make_index_sequence<sizeof...(Ts)>());
+}
+
+namespace detail {
+struct hash_bytes_directly_tag {};
+struct hash_by_iterating_tag {};
+
+template <typename C>
+struct select_hash_iteration_strategy {
+  using type = hash_by_iterating_tag;
+};
+
+template <typename T>
+struct select_hash_iteration_strategy<vector<T>> {
+  using type = conditional_t<is_uniquely_represented<T>::value,
+                             hash_bytes_directly_tag, hash_by_iterating_tag>;
+};
+
+template <typename HashAlgorithm, typename C>
+void hash_append_container(
+    HashAlgorithm& h, const C& c, hash_bytes_directly_tag) {
+  if (!c.empty()) {
+    h(&*c.begin(), c.size() * sizeof(*c.begin()));
+  }
+  hash_append(h, static_cast<size_t>(c.size()));
+}
+
+template <typename HashAlgorithm, typename C>
+void hash_append_container(
+    HashAlgorithm& h, const C& c, hash_by_iterating_tag) {
+  for (const auto& v : c) {
+    hash_append(h, v);
+  }
+}
+}  // namespace detail
+
+template <typename HashAlgorithm, typename T>
+void hash_append(HashAlgorithm& h, const vector<T>& v) {
+  detail::hash_append_container(
+      h, v, typename detail::select_hash_iteration_strategy<vector<T>>::type());
 }
 
 template <typename H>
